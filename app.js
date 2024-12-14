12cb27763c2195ac79c5cbc1411aff102d0ae90d
Marquesgui22 <guilherme.gm522@gmail.com>
2024-12-14 15:18:38 -0300

    sengundo commit

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 // enquanto o chute nao for igual n.s. vai continuar rodando o jogo
 while (chute != numeroSecreto) {
-        chute = prompt('Escolha um número entre 1 e 10');
-        // se chute for igual ao número secreto
-        if (chute == numeroSecreto) {
-            alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+                
     } else {
-            if (chute > numeroSecreto) {
-                alert(`O numero secreto é menor que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto é menor que ${chute}`);
         } else {
-            alert(`O numero secreto é maior que ${chute}`)
+            alert(`O numero secreto é maior que ${chute}`);
         }
 
+        // tentativas = tetativas + 1;
+        tentativas++;
     }
 }    
+
+let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
